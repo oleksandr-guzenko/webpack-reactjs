0bc1d168adcd262453e111923a842c021a02a0ec
DragonWarrior980706 <dragon.warrior980706@gmail.com>
2021-01-02 13:48:45 +0100

    made some changes

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 var path = require("path");
 var express = require("express");
-
+var cors = require('cors')
 var DIST_DIR = path.join(__dirname, "build");
 var PORT = 3000;
 var app = express();
-
+app.use(cors())
+app.options('*', cors()) // include before other routes
 //Serving the files on the dist folder
 app.use(express.static(DIST_DIR));
 
@@ -13,4 +14,6 @@ app.get("*", function (req, res) {
   res.sendFile(path.join(DIST_DIR, "index.html"));
 });
 
-app.listen(PORT);
+app.listen(process.env.PORT || 5100, () => {
+  console.log('http://localhost:5100')
+});
