622f125c98f0068cd9434d6b02daea6243e796c6
DragonWarrior980706 <dragon.warrior980706@gmail.com>
2021-01-02 15:53:01 +0100

    changes

--- server.js
+++ server.js
@@ -1,18 +1,16 @@
-var path = require("path");
-var express = require("express");
-var cors = require('cors')
-var app = express();
-var DIST_DIR = path.join(__dirname, "build");
-app.use(cors())
-app.options('*', cors()) // include before other routes
-//Serving the files on the dist folder
-app.use(express.static(DIST_DIR));
-
-//Send index.html when the user access the web
-app.get("*", function (req, res) {
-  res.sendFile(path.join(DIST_DIR, "index.html"));
+const express = require('express');
+const favicon = require('express-favicon');
+const path = require('path');
+const port = process.env.PORT || 8080;
+const app = express();
+app.use(favicon(__dirname + '/build/favicon.ico'));
+// the __dirname is the current directory from where the script is running
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('/ping', function (req, res) {
+ return res.send('pong');
 });
-
-app.listen(process.env.PORT || 5000, () => {
-  console.log('http://localhost:5000')
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
+app.listen(port);
