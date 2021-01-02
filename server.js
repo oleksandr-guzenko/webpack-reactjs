af11a48112da7f0865bc91ba0bba8649ab1c5cde
DragonWarrior980706 <dragon.warrior980706@gmail.com>
2021-01-02 15:52:26 +0100

    changes

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var path = require("path");
 var express = require("express");
 var cors = require('cors')
 var app = express();
+var DIST_DIR = path.join(__dirname, "build");
 app.use(cors())
 app.options('*', cors()) // include before other routes
 //Serving the files on the dist folder
@@ -9,9 +10,9 @@ app.use(express.static(DIST_DIR));
 
 //Send index.html when the user access the web
 app.get("*", function (req, res) {
-  res.sendFile(path.join(path.join(__dirname+'/client/build/index.html')));
+  res.sendFile(path.join(DIST_DIR, "index.html"));
 });
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log('http://localhost:3000')
+app.listen(process.env.PORT || 5000, () => {
+  console.log('http://localhost:5000')
 });
