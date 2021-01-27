d8a0a41e6aa5d7f1a340797af30ec7efb031bafc
DragonWarrior980706 <dragon.warrior980706@gmail.com>
2021-01-27 14:14:02 +0100

    country got animation now

--- src/Components/RenderPlayer1Country.jsx
+++ src/Components/RenderPlayer1Country.jsx
@@ -6,26 +6,9 @@ function RenderPlayer1Country(props) {
     [countryP1, updateCountryP1] = useState(''),
     [countryP2, updateCountryP2] = useState(''),
     [swapPlace, updateSwapPlace] = useState(true),
+    [animationFlagP1, updateAnimationFlagP1] = useState(''),
+    [animationFlagP2, updateAnimationFlagP2] = useState(''),
     socket = io.connect(props.mode);
-
-  const renderCountryOfPlayer = (cP1, cP2) => {
-    if (swapPlace) {
-      if (cP1 === '' || cP1 === 'Player-1' || cP1 === undefined) {
-        return <img className="img-p1 default-flag" src={`${flag}`} alt="flag" />
-      }
-      return (
-        <img className="img-p1" src={`${cP1}`} alt="Country flag P1" />
-      )
-    }
-    else if (swapPlace === false) {
-      if (cP2 === '' || cP2 === 'Player-2' || cP2 === undefined) {
-        return <img className="img-p2 swaped" src={`${flag}`} alt="flag" />
-      }
-      return (
-        <img className="img-p2 swaped" src={`${cP2}`} alt="Country flag P2" />
-      )
-    }
-  }
 
   useEffect(() => {
     socket.on("swap-place", (swap) => {
@@ -33,19 +16,49 @@ function RenderPlayer1Country(props) {
     })
 
     socket.on("player1country", (country) => {
+      if (countryP1 != country) {
+        updateAnimationFlagP1('')
+        setTimeout(() => {
+          updateAnimationFlagP1('animation-time-flag-P1')
+        }, 250)
+      }
       updateCountryP1(country)
     })
 
     socket.on("player2country", (country) => {
+      if (countryP2 != country) {
+        updateAnimationFlagP2('')
+        setTimeout(() => {
+          updateAnimationFlagP2('animation-time-flag-P2')
+        }, 250)
+      }
       updateCountryP2(country)
     })
   }, []);
 
+  const renderCountryOfPlayer = (cP1, cP2) => {
+    if (swapPlace) {
+      if (cP1 === '' || cP1 === 'Player-1' || cP1 === undefined) {
+        return <img src={`${flag}`} alt="flag" />
+      }
+      return (
+        <img className={`img-p1 ${animationFlagP1}`} src={`${cP1}`} alt="Country flag P1" />
+      )
+    }
+    else if (swapPlace === false) {
+      if (cP2 === '' || cP2 === 'Player-2' || cP2 === undefined) {
+        return <img className="swaped" src={`${flag}`} alt="flag" />
+      }
+      return (
+        <img className={`img-p2 swaped ${animationFlagP2}`} src={`${cP2}`} alt="Country flag P2" />
+      )
+    }
+  }
+
+
   return (
     <div className="P1-country">
-      {/* <div className="shape hex"> */}
-        {renderCountryOfPlayer(countryP1, countryP2)}
-      {/* </div> */}
+      {renderCountryOfPlayer(countryP1, countryP2)}
     </div>
   );
 }
--- src/Components/RenderPlayer2Country.jsx
+++ src/Components/RenderPlayer2Country.jsx
@@ -7,27 +7,9 @@ function RenderPlayer2Country(props) {
     [countryP1, updateCountryP1] = useState(''),
     [countryP2, updateCountryP2] = useState(''),
     [swapPlace, updateSwapPlace] = useState(true),
+    [animationFlagP1, updateAnimationFlagP1] = useState(''),
+    [animationFlagP2, updateAnimationFlagP2] = useState(''),
     socket = io.connect(props.mode);
-
-  const renderCountryOfPlayer = (cP1, cP2) => {
-    if (swapPlace) {
-      if (cP2 === '' || cP2 === 'Player-2' || cP2 === undefined) {
-        return <img className="img-p2 default-flag" src={`${flag}`} alt="flag" />
-      }
-      return (
-        <img className="img-p2" src={`${cP2}`} alt="Country flag P2" />
-      )
-    }
-    else if (swapPlace === false) {
-      if (cP1 === '' || cP1 === 'Player-1' || cP1 === undefined) {
-        return <img className="img-p1 swaped" src={`${flag}`} alt="flag" />
-      }
-      return (
-        <img className="img-p1 swaped" src={`${cP1}`} alt="Country flag P1" />
-      )
-    }
-  }
-
 
   useEffect(() => {
     socket.on("swap-place", (swap) => {
@@ -35,18 +17,50 @@ function RenderPlayer2Country(props) {
     })
 
     socket.on("player1country", (country) => {
+      if (countryP1 != country) {
+        updateAnimationFlagP1('')
+        setTimeout(() => {
+          updateAnimationFlagP1('animation-time-flag-P1')
+        }, 250)
+      }
       updateCountryP1(country)
     })
 
     socket.on("player2country", (country) => {
+      if (countryP2 != country) {
+        updateAnimationFlagP2('')
+        setTimeout(() => {
+          updateAnimationFlagP2('animation-time-flag-P2')
+        }, 250)
+      }
       updateCountryP2(country)
     })
   }, []);
 
+
+  const renderCountryOfPlayer = (cP1, cP2) => {
+    if (swapPlace) {
+      if (cP2 === '' || cP2 === 'Player-2' || cP2 === undefined) {
+        return <img src={`${flag}`} alt="flag" />
+      }
+      return (
+        <img className={`img-p2 ${animationFlagP2}`} src={`${cP2}`} alt="Country flag P2" />
+      )
+    }
+    else if (swapPlace === false) {
+      if (cP1 === '' || cP1 === 'Player-1' || cP1 === undefined) {
+        return <img className="swaped" src={`${flag}`} alt="flag" />
+      }
+      return (
+        <img className={`img-p1 swapped ${animationFlagP1}`}src={`${cP1}`} alt="Country flag P1" />
+      )
+    }
+  }
+
   return (
     <div className="P2-country">
       {/* <div className="shape hex"> */}
-        {renderCountryOfPlayer(countryP1, countryP2)}
+      {renderCountryOfPlayer(countryP1, countryP2)}
       {/* </div> */}
     </div>
   );
